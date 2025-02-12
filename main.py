bbc9f873d953d3a7f576a7be7b5a467a2a59e02c
Ricardo Ibarra <ribarra.delgado@iteso.mx>
2025-02-11 19:49:50 -0600

    Commit avance

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import numpy as np
 from scipy.integrate import quad
 from scipy.stats import weibull_min as wbl, weibull_min
 import matplotlib.pyplot as plt
-from scenario1 import bid_ask
-from scenario2 import revenue_scenario2 as r2
+import bid_ask
 
-lambda_val = 50
-k_shape = 10
-s_range = np.linspace(0, 10, 100)
+lambda_ = 50
+k = 10
+s_range = np.linspace(0, 100, 1000)
+p0 = 51
 
-funct = bid_ask()
+bid = p0 - 5
+ask = p0 + 5
 
-prices = weibull_min.rvs(k_shape, scale=lambda_val, size=100)
+
+funct = bid_ask.weibull(s_range, lambda_, k)
+
+
 
 plt.figure(figsize=(8, 5))
 plt.plot(s_range, funct, label="Liquidity Motivated Trades (Pi_I = 0)", linestyle="--")
@@ -20,3 +24,6 @@ plt.ylabel("Expected Revenue")
 plt.legend()
 plt.title("Expected Revenue under Different Trading Scenarios")
 plt.show()
+
+
+pi_inf
